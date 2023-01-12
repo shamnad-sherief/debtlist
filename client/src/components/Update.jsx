bade2b91adeda430048f74ed611e4b7866f5e66f
shamnad-sherief <sshamnad03@gmail.com>
2023-01-13 00:24:26 +0530

    add: Update

--- client/src/components/Update.jsx
+++ client/src/components/Update.jsx
@@ -1,8 +1,45 @@
+import axios from 'axios';
 import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import './styles.css'
 
-const Update = () => {
+
+export const Update = () => {
+
+    const [list,setList] = useState({
+        name:"",
+        amount:"",
+    });
+
+  const navigate = useNavigate() 
+
+  const listId = window.location.pathname.split( '/' )[2]
+
+  const handleChange = (e) =>{
+  setList((prev) => ({ ...prev, [e.target.name]: e.target.value}));
+  };
+
+
+  const handleClick = async (e) =>{
+    e.preventDefault()
+    
+    try{
+        await axios.put("http://localhost:8800/lists/"+listId,list)
+        navigate("/")
+    } catch(err){
+        console.log(err)
+    }
+  }
+
+  console.log(list)
   return (
-    <div>Update</div>
+    <div className="form">
+        <h1>Update List</h1>
+        <input type='text' placeholder='name' onChange={handleChange} name='name'></input>
+        <input type='text' placeholder='amount' onChange={handleChange} name='amount'></input>
+     <button onClick={handleClick}>Update</button>
+    </div>
   )
 }
 

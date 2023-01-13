6216a186b58edb0cd4775430135590ee2bf58105
shamnad-sherief <sshamnad03@gmail.com>
2023-01-13 14:20:48 +0530

    fix: table

--- client/src/components/DebtList.jsx
+++ client/src/components/DebtList.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Table } from 'semantic-ui-react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import './styles.css'
 
 const DebtList = () => {
@@ -31,32 +32,49 @@ const DebtList = () => {
   }
 
   return (
-    <div className="debt-list-container">
-
-        <h1>DebtList</h1>
+   <DragDropContext>
+     <div className="debt-list-container">
+     <h1>DebtList</h1>
         <Table className='table-container'>
-          <Table.Header>
+        <Table.Header>
             <Table.Row>
-              <Table.HeaderCell>Name</Table.HeaderCell>
-              <Table.HeaderCell>Amount</Table.HeaderCell>
-              <Table.HeaderCell colspan="2">Actions</Table.HeaderCell>
+            <Table.HeaderCell>Name</Table.HeaderCell>
+            <Table.HeaderCell>Amount</Table.HeaderCell>
+            <Table.HeaderCell colSpan="2">Actions</Table.HeaderCell>
             </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {lists.map(list => (
-              <Table.Row key={list.id}>
+        </Table.Header>
+        <Droppable droppableId="tbody">
+        {
+            (provided) => (
+                <Table.Body ref={provided.innerRef} {...provided.droppableProps}>
+            {lists.map((list, index) => (
+            <Draggable draggableId={index.toString()}>
+                {
+                    (provided) => (
+                        <Table.Row key={list.id} ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}>
                 <Table.Cell>{list.name}</Table.Cell>
                 <Table.Cell>{list.amount}</Table.Cell>
                 <Table.Cell><button id='edit-button' ><Link to={{ pathname: `/update/${list.id}`,  state: { name: list.name, amount: list.amount }, }} id='edit' className="button">Edit</Link></button></Table.Cell>
                 <Table.Cell><button id='delete-button' className="button" onClick={()=>handleDelete(list.id)}>Delete</button></Table.Cell>
-
-              </Table.Row>
+            </Table.Row>
+                    )
+                }
+                
+            </Draggable>
             ))}
-          </Table.Body>
+        </Table.Body>
+            )
+        }
+
+        </Droppable>
         </Table>
+
             <Link to="/add" id="submit"> Add new List</Link>
-    </div>
-  )
+        </div>
+   </DragDropContext>
+  );
 }
 
 export default DebtList

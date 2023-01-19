a127f85fd4b8e69556a94ac0615789b1fb28ce50
shamnad-sherief <sshamnad03@gmail.com>
2023-01-19 15:44:43 +0530

    add: route for users

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,18 @@ import {
 import DebtList from "./components/DebtList";
 import Add from "./components/Add";
 import Update from "./components/Update";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
-        <Route path="/" element={<DebtList/>}/>
-        <Route path="/add" element={<Add/>}/>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/debtlist" element={<DebtList/>}/>
+        <Route path="/add/:userId" element={<Add/>}/>
         <Route path="/update/:id" element={<Update/>}/>
 
       </Routes>
